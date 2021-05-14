17c3c4add3d045dd2a4cdc925e46a0b1f0f5f0fc
Iain Mitchell <iainjmitchell@gmail.com>
2021-05-14 16:21:29 +0100

    starting to add hotspot analysis

--- run-analysis.sh
+++ run-analysis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CODE_DIR='code'
+GIT_LOG='git_log.log'
+CLOC_LOG='cloc_output.csv'
 
 function start() {
     repository=$1
@@ -8,8 +10,10 @@ function start() {
     clean
     get_code
     create_analysis_site
+    generate_git_log
+    count_lines_of_code
     analyse_code 
-    start_server
+    # start_server
 }
 
 function get_code() {
@@ -18,7 +22,7 @@ function get_code() {
 
 function clean() {
     rm -rf $CODE_DIR
-    rm -rf *_analysis
+    rm -rf analysis_*
 }
 
 function create_analysis_site() {
@@ -28,12 +32,30 @@ function create_analysis_site() {
 function analyse_code() {
     echo 'starting analysis'
     churn_analysis
+    hotspots_analysis
 }
 
 function churn_analysis() {
-    git_log='churn_log.log'
-    cd code && git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames > $git_log  && cd ..
-    docker container run -v $(pwd)/code:/code --rm philipssoftware/code-maat -l /code/$git_log -c git2 -a abs-churn > $codebaseFolder/data/churn.csv
+    echo 'churn analysis'
+    docker container run -v $(pwd)/$CODE_DIR:/$CODE_DIR --rm philipssoftware/code-maat -l /$CODE_DIR/$GIT_LOG -c git2 -a abs-churn > $codebaseFolder/data/churn.csv
+}
+
+function hotspots_analysis() {
+    echo 'hotspot analysis'
+    revisions_file='revisions.csv'
+    docker container run -v $(pwd)/$CODE_DIR:/$CODE_DIR --rm philipssoftware/code-maat -l /$CODE_DIR/$GIT_LOG -c git2 -a revisions > ./$CODE_DIR/$revisions_file
+    echo 'python ./scripts/csv_as_enclosure_json.py --structure ../$CODE_DIR/$CLOC_LOG --weights ../$CODE_DIR/$revisions_file > $codebaseFolder/data/hotspots.json'
+    python ./scripts/csv_as_enclosure_json.py --structure $CODE_DIR/$CLOC_LOG --weights $CODE_DIR/$revisions_file > $codebaseFolder/data/hotspots.json
+}
+
+function generate_git_log(){
+    cd $CODE_DIR 
+    git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames > $GIT_LOG
+    cd ..
+}
+
+function count_lines_of_code() {
+    docker run --rm -v $(pwd)/$CODE_DIR:/tmp aldanial/cloc /tmp/. --by-file --csv --report-file=$CLOC_LOG
 }
 
 function start_server(){
