287e411f5457586e87185fe307dc0be838380075
Iain Mitchell <iainjmitchell@gmail.com>
2021-05-20 16:12:24 +0100

    refactoring

--- scripts/test/tranforms/hotspot-to-d3-json.test.js
+++ scripts/test/tranforms/hotspot-to-d3-json.test.js
@@ -1,33 +1,39 @@
-const EOL = require('os').EOL
+const EOL = require('os').EOL;
 
 const testCases = [
     { module: 'file1.js', revisions: 50, code: 1341 },
     { module: 'file2.js', revisions: 80, code: 9999 }
-]
-
+];
 testCases.forEach((testCase) => {
     test('converted one record with 100% of weight', async() => {
         const hotspotCSVData = [
             'module,revisions,code',
             `${testCase.module},${testCase.revisions},${testCase.code}`
-        ].join(EOL)
+        ].join(EOL);
     
-        await expect(HotspotToD3Json.transform(hotspotCSVData)).resolves.toEqual({"size": testCase.code, "name": testCase.module, "weight": 1})
+        await expect(HotspotToD3Json.transform(hotspotCSVData)).resolves.toEqual({"size": testCase.code, "name": testCase.module, "weight": 1});
     })
-})
+});
 
-const parseString = require('@fast-csv/parse').parseString
+const parseString = require('@fast-csv/parse').parseString;
 
-const HotspotToD3Json = {
-    transform: async(hotspotCSVData) => {
+class HotspotToD3Json {
+    static async transform(hotspotCSVData) {
+        const csvData = await HotspotToD3Json._parseCSVData(hotspotCSVData);
+        return HotspotToD3Json._convertRow(csvData[0]);
+    }
+
+    static async _parseCSVData(hotspotCSVData){
+        const rows = [];
         return new Promise((resolve, reject) => {
-            const rows = []
-            parseString(hotspotCSVData, {headers: true})
-                .on('data', row => {rows.push(row)})
-                .on('end', () => { 
-                    resolve({"size": parseInt(rows[0].code), "name": rows[0].module, "weight": 1})
-                })
-                .on('error', reject)
-        })
+            parseString(hotspotCSVData, { headers: true })
+                .on('data', row => rows.push(row))
+                .on('end', () => resolve(rows))
+                .on('error', reject);
+        });
+    }
+
+    static _convertRow(csvDataRow){
+        return {"size": parseInt(csvDataRow.code), "name": csvDataRow.module, "weight": 1}
     }
 }
