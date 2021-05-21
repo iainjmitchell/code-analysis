843a58b140a02b35da9075c8f36e22f8095bc72d
Iain Mitchell <iainjmitchell@gmail.com>
2021-05-21 09:43:00 +0100

    dealing with multiple files and spread revisions

--- scripts/test/tranforms/hotspot-to-d3-json.test.js
+++ scripts/test/tranforms/hotspot-to-d3-json.test.js
@@ -28,15 +28,36 @@ test('converted two root records with equal split of weight', async() => {
     
     const hotspotCSVData = [
         'module,revisions,code',
-        `${record[0].module},${record[0].revisions},${record[0].code}`,
-        `${record[1].module},${record[1].revisions},${record[1].code}`
+        `${records[0].module},${records[0].revisions},${records[0].code}`,
+        `${records[1].module},${records[1].revisions},${records[1].code}`
     ].join(EOL);
 
     await expect(HotspotToD3Json.transform(hotspotCSVData)).resolves.toEqual({
         "name": ".",
         "children": [
-            {"size": record[0].code, "name": record[0].module, "weight": 0.5},
-            {"size": record[1].code, "name": record[1].module, "weight": 0.5}
+            {"size": records[0].code, "name": records[0].module, "weight": 0.5},
+            {"size": records[1].code, "name": records[1].module, "weight": 0.5}
+        ]
+    });
+});
+
+test('converted two root records with unequal split of weight', async() => {
+    const records = [
+        { module: 'file1.js', revisions: 150, code: 100 },
+        { module: 'file2.js', revisions: 50, code: 200 },
+    ]
+    
+    const hotspotCSVData = [
+        'module,revisions,code',
+        `${records[0].module},${records[0].revisions},${records[0].code}`,
+        `${records[1].module},${records[1].revisions},${records[1].code}`
+    ].join(EOL);
+
+    await expect(HotspotToD3Json.transform(hotspotCSVData)).resolves.toEqual({
+        "name": ".",
+        "children": [
+            {"size": records[0].code, "name": records[0].module, "weight": 0.75},
+            {"size": records[1].code, "name": records[1].module, "weight": 0.25}
         ]
     });
 });
@@ -46,7 +67,11 @@ const parseString = require('@fast-csv/parse').parseString;
 class HotspotToD3Json {
     static async transform(hotspotCSVData) {
         const csvData = await HotspotToD3Json._parseCSVData(hotspotCSVData);
-        return HotspotToD3Json._convertRow(csvData[0]);
+        const totalNumberOfRevisions = csvData.reduce((accumilator, csvRow) => accumilator + parseInt(csvRow.revisions), 0);
+        return {
+            "name": ".",
+            "children": csvData.map(csvRow => HotspotToD3Json._convertRow(csvRow, totalNumberOfRevisions))
+        };
     }
 
     static async _parseCSVData(hotspotCSVData){
@@ -59,12 +84,9 @@ class HotspotToD3Json {
         });
     }
 
-    static _convertRow(csvDataRow){
-        return {
-            "name": ".",
-            "children": [
-                {"size": parseInt(csvDataRow.code), "name": csvDataRow.module, "weight": 1}
-            ]
-        }
+    static _convertRow(csvDataRow, totalNumberOfRevisions){
+        const revisions = parseInt(csvDataRow.revisions);
+        const weight = (revisions / totalNumberOfRevisions);
+        return {"size": parseInt(csvDataRow.code), "name": csvDataRow.module, "weight": weight};
     }
 }
